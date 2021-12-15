20bda0a7086527405df856cd1639c07557cc0ebc
mhmmdq <mhmmdq@mhmmdq.ir>
2021-12-15 22:10:17 +0330

    update users migaration

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\User;
 class LoginController extends Controller
 {
-    //
+    public function login(Request $request) {
+        
+    }
+
+    public function singup() {
+        
+    }
 }
